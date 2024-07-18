d92c4bbdf150603a9a59e315075a41e68952c48e
jagdish575 <151916824+jagdish575@users.noreply.github.com>
2024-07-18 12:13:17 +0530

    add

--- daily_commit.bat
+++ daily_commit.bat
@@ -6,7 +6,7 @@ cd /d "C:\Users\jagdi\Videos\github automation"
 REM Create or update a file with the current date and time
 echo Last update: %date% %time% > daily_update.txt
 
-REM Add the file to the staging area
+REM Add the file to the staging areagit
 git add daily_update.txt
 
 REM Commit the changes with a message
